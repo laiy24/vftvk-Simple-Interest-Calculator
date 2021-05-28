5254a78b1731ecacb57c63ed6f6703e7f230342d
laiy24 <71063139+laiy24@users.noreply.github.com>
2021-05-28 23:53:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function compute()
 {
-    prin = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    year = document.getElementById("year").value;
-    endresult = prin*(1+rate)**year;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("year").value;
+    var interest = principal*(1+rate)**years;
+    
+    var futureyear = parseInt(years) + 2020
+    var result = document.getElementById("result");
+    
+    // Displays text in inner.HTML with breaks and highlights
+    if(principal >0){
+    result.innerHTML = "If you deposit <span class='highlight'>"+ principal + "</span> ,<br> at an interest rate of <span class='highlight'>" + rate + "</span>% <br> You will receive an amount of <span class='highlight'>" + interest + "</span>, <br> in the year <span class='highlight'>" + futureyear +"</span>" 
+    }
+    else{ 
+    // Validation shows alert if input is zero or negative number
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    
+      } 
 }
+function setSliderLabel() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rate_span").innerHTML = rate + "%";
+ }      
         
